d1c12ed16fb9a8fcaa4b69b00406e7fcfabed15e
Jeff Eldredge <jdeldre@g.ucla.edu>
2023-11-06 21:57:43 -0800

    Fixed tests

--- test/generatedfields.jl
+++ test/generatedfields.jl
@@ -9,7 +9,7 @@ g = EmptySpatialField()
 for x in [-0.5,0,0.5], y in [-0.5,0,0.5]
   g += SpatialGaussian(0.2,0.5,x,y,1)
 end
-@test g(0.5,0) ≈ 5.5357580598289235
+@test g(0.5,0) ≈ 3.6769641226297485
 
 
 gr = PhysicalGrid((-3.0,3.0),(-2.0,2.0),0.03)
@@ -24,7 +24,7 @@ gfield = GeneratedField(w,g,gr)
 
 wfield = PulseField(gfield,0.5,0.1)
 
-@test maximum(wfield(0.5)) ≈ maximum(gfield()) ≈ 5.532960088678624
+@test maximum(wfield(0.5)) ≈ maximum(gfield()) ≈ 3.825607182823737
 @test maximum(wfield(2)) ≈ 0.0
 @test datatype(wfield) == datatype(gfield) <: ScalarGridData
 
@@ -44,13 +44,13 @@ xc, yc = coordinates(w,gr)
 
 
 q = Edges(Primal,size(gr))
-gauss = SpatialGaussian(0.5,0,0,1)
+gauss = SpatialGaussian(0.5,0.5,0,0,1)
 gaussfield = GeneratedField(q,gauss,EmptySpatialField(),gr)
 q .= gaussfield()
 
 ffield = PulseField(gaussfield,0.5,0.1)
 
-@test maximum(ffield(0.5).u) ≈ maximum(q.u) ≈ 1.272094144652253
+@test maximum(ffield(0.5).u) ≈ maximum(q.u) ≈ 0.6363333579181004
 @test maximum(ffield(2).u) ≈ 0.0
 @test maximum(ffield(0.5).v) ≈ maximum(q.v) ≈ 0.0
 @test maximum(ffield(2).v) ≈ maximum(q.v) ≈ 0.0
@@ -60,8 +60,8 @@ ffield = PulseField(gaussfield,0.5,0.1)
 gq = EdgeGradient(Dual,size(gr))
 tfield = GeneratedField(gq,[g,gauss,EmptySpatialField(),EmptySpatialField()],gr)
 gq .= tfield()
-@test maximum(gq.dudx) ≈ 5.532960088678624
-@test maximum(gq.dudy) ≈ 1.2732395447351625
+@test maximum(gq.dudx) ≈ 3.825607182823737
+@test maximum(gq.dudy) ≈ 0.6366197723675814
 @test maximum(gq.dvdx) == maximum(gq.dvdy) == 0.0
 
 ffield = PulseField(tfield,0.5,0.1)
