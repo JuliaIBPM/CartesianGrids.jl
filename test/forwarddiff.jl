87ab4c140c2d13615f6d45fef921f8419a36d7cf
Hanieh Mousavi <hanieh.mousavi93@gmail.com>
2024-01-24 14:44:09 -0800

    modified test file

--- test/forwarddiff.jl
+++ test/forwarddiff.jl
@@ -1,25 +1,26 @@
 using ForwardDiff
 FD = ForwardDiff
 
-Δx = 0.2
-Lx = 1.0
-xlim = (-Lx,Lx)
-ylim = (-Lx,Lx)
-g = PhysicalGrid(xlim,ylim,Δx)
-w = Nodes(Primal,size(g))
-xg, yg = coordinates(w,g)
-x = collect(xg)
-y = collect(yg)
+@testset "Regularization accepting Dual type numbers" begin
+    Δx = 0.2
+    Lx = 1.0
+    xlim = (-Lx,Lx)
+    ylim = (-Lx,Lx)
+    g = PhysicalGrid(xlim,ylim,Δx)
+    w = Nodes(Primal,size(g))
+    xg, yg = coordinates(w,g)
+    x = collect(xg)
+    y = collect(yg)
 
-X = zeros(length(x)+length(y))
-for i in eachindex(x)
-    X[2i-1] = x[i]
-    X[2i] = y[i]
-end
-cfg = FD.JacobianConfig(f, X)
-Xdual = cfg.duals
-xdual = Xdual[1:2:end]
-ydual = Xdual[2:2:end]
+    X = zeros(length(x)+length(y))
+    for i in eachindex(x)
+        X[2i-1] = x[i]
+        X[2i] = y[i]
+    end
+    cfg = FD.JacobianConfig(f, X)
+    Xdual = cfg.duals
+    xdual = Xdual[1:2:end]
+    ydual = Xdual[2:2:end]
 
-H = Regularize(xdual,ydual,cellsize(g),I0=origin(g),issymmetric=true)
-
+    H = Regularize(xdual,ydual,cellsize(g),I0=origin(g),issymmetric=true)
+end 
