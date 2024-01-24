d02556e3166ffcdf8d811b80cf056db1e6746159
Hanieh Mousavi <hanieh.mousavi93@gmail.com>
2024-01-24 12:00:30 -0800

    set test for Regularization with dual numbers as inputs

--- test/forwarddiff.jl
+++ test/forwarddiff.jl
@@ -1,3 +1,25 @@
 using ForwardDiff
 FD = ForwardDiff
 
+Δx = 0.2
+Lx = 1.0
+xlim = (-Lx,Lx)
+ylim = (-Lx,Lx)
+g = PhysicalGrid(xlim,ylim,Δx)
+w = Nodes(Primal,size(g))
+xg, yg = coordinates(w,g)
+x = collect(xg)
+y = collect(yg)
+
+X = zeros(length(x)+length(y))
+for i in eachindex(x)
+    X[2i-1] = x[i]
+    X[2i] = y[i]
+end
+cfg = FD.JacobianConfig(f, X)
+Xdual = cfg.duals
+xdual = Xdual[1:2:end]
+ydual = Xdual[2:2:end]
+
+H = Regularize(xdual,ydual,cellsize(g),I0=origin(g),issymmetric=true)
+
