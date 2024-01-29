b4784d24857594d0f2345b7a7ff4b77b30c8a324
Hanieh Mousavi <hanieh.mousavi93@gmail.com>
2024-01-29 14:25:16 -0800

    changed real to Float64

--- src/gridoperations/laplacian.jl
+++ src/gridoperations/laplacian.jl
@@ -284,14 +284,14 @@ for (lf,inplace) in ((:plan_laplacian,false),
     end
 
     @eval function $lf(nx::Int, ny::Int;
-        with_inverse = false, fftw_flags = FFTW.ESTIMATE, factor = 1.0, dx = 1.0, dtype = Real, nthreads = MAX_NTHREADS)
+        with_inverse = false, fftw_flags = FFTW.ESTIMATE, factor = 1.0, dx = 1.0, dtype = Float64, nthreads = MAX_NTHREADS)
         $lf((nx, ny), with_inverse = with_inverse, fftw_flags = fftw_flags, factor = factor, dx = dx, dtype = dtype, nthreads = nthreads)
     end
 
     # Base the size on the dual grid associated with any grid data, since this
     # is what the efficient grid size in PhysicalGrid has been established with
     @eval function $lf(::GridData{NX,NY};
-        with_inverse = false, fftw_flags = FFTW.ESTIMATE, factor = 1.0, dx = 1.0, dtype = Real, nthreads = MAX_NTHREADS) where {NX,NY}
+        with_inverse = false, fftw_flags = FFTW.ESTIMATE, factor = 1.0, dx = 1.0, dtype = Float64, nthreads = MAX_NTHREADS) where {NX,NY}
         $lf((NX,NY), with_inverse = with_inverse, fftw_flags = fftw_flags, factor = factor, dx = dx, dtype = dtype, nthreads = nthreads)
     end
 end
