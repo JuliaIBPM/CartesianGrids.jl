5c2ac5cb7f1f4aae257cce6add122fe5bc7a8882
Hanieh Mousavi <hanieh.mousavi93@gmail.com>
2024-01-29 17:51:00 -0800

    change all Real to Float64 in the definitions of all DDF

--- src/points/ddf.jl
+++ src/points/ddf.jl
@@ -54,33 +54,33 @@ macro ddffunc(ddftype)
     dfname = Symbol("ddf_d",lowercase(string(ddftype)))
     return esc(quote
             abstract type $ddftype <: DDFType end
-            @inline (::DDF{$ddftype,OVERDX})(x::T) where {OVERDX,T <: Real} =
+            @inline (::DDF{$ddftype,OVERDX})(x::T) where {OVERDX,T <: Float64} =
                   $fname(abs(x)*OVERDX)*OVERDX
-            @inline (::DDF{$ddftype,OVERDX})(x::T,y::T) where {OVERDX,T <: Real} =
+            @inline (::DDF{$ddftype,OVERDX})(x::T,y::T) where {OVERDX,T <: Float64} =
                           $fname(abs(x)*OVERDX)*OVERDX*$fname(abs(y)*OVERDX)*OVERDX
-            @inline (::DDF{$ddftype,OVERDX})(x::T,y::T,z::T) where {OVERDX,T <: Real} =
+            @inline (::DDF{$ddftype,OVERDX})(x::T,y::T,z::T) where {OVERDX,T <: Float64} =
                           $fname(abs(x)*OVERDX)*OVERDX*$fname(abs(y)*OVERDX)*OVERDX*$fname(abs(z)*OVERDX)*OVERDX
 
             @inline (d::DDF{$ddftype,OVERDX})(x::AbstractVector{T},y::AbstractVector{T}) where {OVERDX,T <: Float64} =
                           d.(x)*d.(y')
 
-            @inline (::GradDDF{$ddftype,OVERDX,N})(x::T) where {OVERDX,N,T <: Real} =
+            @inline (::GradDDF{$ddftype,OVERDX,N})(x::T) where {OVERDX,N,T <: Float64} =
                    $dfname(x*OVERDX)*OVERDX*OVERDX
-            @inline (::GradDDF{$ddftype,OVERDX,1})(x::T,y::T) where {OVERDX,T <: Real} =
+            @inline (::GradDDF{$ddftype,OVERDX,1})(x::T,y::T) where {OVERDX,T <: Float64} =
                    $dfname(x*OVERDX)*OVERDX*OVERDX*$fname(abs(y)*OVERDX)*OVERDX
-            @inline (::GradDDF{$ddftype,OVERDX,2})(x::T,y::T) where {OVERDX,T <: Real} =
+            @inline (::GradDDF{$ddftype,OVERDX,2})(x::T,y::T) where {OVERDX,T <: Float64} =
                    $fname(abs(x)*OVERDX)*OVERDX*$dfname(y*OVERDX)*OVERDX*OVERDX
 
-            @inline (dd::GradDDF{$ddftype,OVERDX,1})(x::AbstractVector{T},y::AbstractVector{T}) where {OVERDX,T <: Real} =
+            @inline (dd::GradDDF{$ddftype,OVERDX,1})(x::AbstractVector{T},y::AbstractVector{T}) where {OVERDX,T <: Float64} =
                           dd.(x)*$fname.(abs.(y')*OVERDX)*OVERDX
-            @inline (dd::GradDDF{$ddftype,OVERDX,2})(x::AbstractVector{T},y::AbstractVector{T}) where {OVERDX,T <: Real} =
+            @inline (dd::GradDDF{$ddftype,OVERDX,2})(x::AbstractVector{T},y::AbstractVector{T}) where {OVERDX,T <: Float64} =
                           $fname.(abs.(x)*OVERDX)*OVERDX*dd.(y')
 
-            @inline (::GradDDF{$ddftype,OVERDX,1})(x::T,y::T,z::T) where {OVERDX,T <: Real} =
+            @inline (::GradDDF{$ddftype,OVERDX,1})(x::T,y::T,z::T) where {OVERDX,T <: Float64} =
                    $dfname(x*OVERDX)*OVERDX*OVERDX*$fname(abs(y)*OVERDX)*OVERDX*$fname(abs(z)*OVERDX)*OVERDX
-            @inline (::GradDDF{$ddftype,OVERDX,2})(x::T,y::T,z::T) where {OVERDX,T <: Real} =
+            @inline (::GradDDF{$ddftype,OVERDX,2})(x::T,y::T,z::T) where {OVERDX,T <: Float64} =
                    $fname(abs(x)*OVERDX)*OVERDX*$dfname(y*OVERDX)*OVERDX*OVERDX*$fname(abs(z)*OVERDX)*OVERDX
-            @inline (::GradDDF{$ddftype,OVERDX,3})(x::T,y::T,z::T) where {OVERDX,T <: Real} =
+            @inline (::GradDDF{$ddftype,OVERDX,3})(x::T,y::T,z::T) where {OVERDX,T <: Float64} =
                    $fname(abs(x)*OVERDX)*OVERDX*$fname(abs(y)*OVERDX)*OVERDX*$dfname(z*OVERDX)*OVERDX*OVERDX
             end)
 end
