fb0c4e6ad11af563747a4033adbc5bdce774a9bd
Hanieh Mousavi <hanieh.mousavi93@gmail.com>
2024-02-06 14:00:54 -0800

    modified L*s for inplace=true accepting FD.Dual numbers

--- src/gridoperations/laplacian.jl
+++ src/gridoperations/laplacian.jl
@@ -319,14 +319,14 @@ mul!(out::T, L::Laplacian, s::T) where T<:GridData = (laplacian!(out, s); out .*
 function (*)(L::Laplacian{MX,MY,T,R,false}, s::GridData) where {MX,MY,T,R}
   parmat = FD.partials.(s.data)
   if any(isempty, parmat)
-    L.factor*laplacian(s)
-    return s
+    return L.factor*laplacian(s)
   end
   # out = deepcopy(s)
   # valmat = FD.value.(s.data)
+  # out.data .= valmat
   # outval = deepcopy(valmat)
-  # mul!(outval,L,s)
-  # out.data .= outval
+  # mul!(out,L,deepcopy(out))
+  # #out.data .= outval
   # idx = findfirst(x -> x != 0, s.data)
   # tag = get_tag(s.data[idx])
   # # matrix including partials of FD.Dual numbers
@@ -351,27 +351,24 @@ function (*)(L::Laplacian{MX,MY,T,R,true}, s::GridData) where {MX,MY,T,R}
     mul!(s,L,deepcopy(s))
     return s
   end
-  # out = deepcopy(s)
-  # valmat = FD.value.(s.data)
-  # outval = deepcopy(valmat)
-  # mul!(outval,L,s)
-  # out.data .= outval
-  # idx = findfirst(x -> x != 0, s.data)
-  # tag = get_tag(s.data[idx])
-  # # matrix including partials of FD.Dual numbers
-  # parval = similar(valmat)
-  # npar = length(parmat[1,1])
-  # outpar = Vector{typeof(parval)}(undef,npar)
+  out = deepcopy(s)
+  out.data .= FD.value.(s.data)
+  mul!(out,L,deepcopy(out))
+  outval = out.data
+  idx = findfirst(x -> x != 0, s.data)
+  tag = get_tag(s.data[idx])
+  # matrix including partials of FD.Dual numbers
+  npar = length(parmat[1,1])
+  outpar = Vector{typeof(outval)}(undef,npar)
 
-  # for k in 1:npar
-  #   fill!(parval, 0)
-  #   parval .= FD.partials.(s.data,k)
-  #   outpar[k] = deepcopy(parval)
-  #   mul!(outpar[k],L,parval)
-  # end
+  for k in 1:npar
+    out.data .= FD.partials.(s.data,k)
+    mul!(out,L,deepcopy(out))
+    outpar[k] = out.data
+  end
 
-  # out.data .= [FD.Dual{tag}(outval[i,j], [outpar[k][i,j] for k in 1:npar]...) for i in 1:size(out.data, 1), j in 1:size(out.data, 2)]
-  # out
+  out.data .= [FD.Dual{tag}(outval[i,j], [outpar[k][i,j] for k in 1:npar]...) for i in 1:size(out.data, 1), j in 1:size(out.data, 2)]
+  out
 end
 
 #=
