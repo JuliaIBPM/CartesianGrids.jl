9fe1a45cbeb523cdd5e4c4cb1e8e2cf8b79802f2
Hanieh Mousavi <hanieh.mousavi93@gmail.com>
2024-03-21 15:48:09 -0700

    removed diffrule.jl

--- src/diffrules.jl
+++
@@ -1,6 +0,0 @@
-# using DiffRules
-
-# DiffRules.@define_diffrule CartesianGrids.(::DDF{ddftype,OVERDX})(x,y) = 
-#     :((GradDDF(1,ddftype=ddftype,dx=1.0/OVERDX))($x,$y)) , :((GradDDF(2,ddftype=ddftype,dx=1.0/OVERDX))($x,$y))
-
-# eval(ForwardDiff.binary_dual_definition(:CartesianGrids, :(::DDF{ddftype,OVERDX})))
